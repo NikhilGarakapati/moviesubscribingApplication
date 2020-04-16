2388ae2c4aef035c0e6ec45aceca1ee398f08145
NikhilGarakapati <nikhilgarakapati@gmail.com>
2020-04-16 19:33:37 +0530

    added token into header

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const _ = require('lodash');
 const {User, validate} = require('../models/user');
 const mongoose = require('mongoose');
@@ -19,14 +21,12 @@ router.post('/', async (req, res) => {
   
   user = new User(_.pick(req.body,['name','email','password']));
   
-  
   // const salt = await bcrypt.genSalt(10);
   // user.password = await bcrypt.hash(user.password, salt);
-
-
   await user.save();
-
-  res.send( _.pick(user, ['_id','name','email']));
+  
+  const token = jwt.sign({ _id: user._id}, config.get('jwtprivateKey'));
+  res.header('x-auth-token', token).send(_.pick(user, ['_id','name','email']));
 });
 
 module.exports = router;
