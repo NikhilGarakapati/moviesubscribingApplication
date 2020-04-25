098c2220baab810f3e66ef721f1d30e7120848f3
NikhilGarakapati <nikhilgarakapati@gmail.com>
2020-04-26 00:00:32 +0530

    80% testing done!

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -11,35 +11,43 @@ describe('/api/genres', ()=>{
     });
 
     describe('GET /',()=>{
-        // it('should return all genres',async ()=>{
-        //     await Genre.collection.insertMany([
-        //         {name: 'genre1'},
-        //         {name: 'genre2'}
-        //     ]);
-        //     const res = await request(server).get('/api/genres');
-        //     expect(res.status).toBe(200);
-        //     expect(res.body.length).toBe(2);
-        //     expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
+        it('should return all genres',async ()=>{
+            await Genre.collection.insertMany([
+                {name: 'genre1'},
+                {name: 'genre2'}
+            ]);
+            const res = await request(server).get('/api/genres');
+            expect(res.status).toBe(200);
+            expect(res.body.length).toBe(2);
+            expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
 
            
-        // });
+        });
     });
 
     describe('GET /:id' ,()=>{
-        // it('should return a genre if valid id is passed', async() => {
-        //     const genre = new Genre({name:'genre1'});
-        //     await genre.save();
+        it('should return a genre if valid id is passed', async() => {
+            const genre = new Genre({name:'genre1'});
+            await genre.save();
 
-        //     const res = request(server).get('/api/genres' + genre._id);
-        //     expect(res.status).toBe(200);
-        //     expect(res.body).toHaveProperty('name', genre.name);
-        // });
-        // it('should return 404 if invalid id is passed', async() => {
+            const res = request(server).get('/api/genres' + genre._id);
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveProperty('name', genre.name);
+        });
+        it('should return 404 if invalid id is passed', async() => {
 
-        //     const res = await request(server).get('/api/genres/1');
-        //     expect(res.status).toBe(404);
+            const res = await request(server).get('/api/genres/1');
+            expect(res.status).toBe(404);
             
-        // });
+        });
+
+        it('should return 404 if no genre with given id exists', async() => {
+
+            const id = mongoose.Types.ObjectID();
+            const res = await request(server).get('/api/genres/'+id);
+            expect(res.status).toBe(404);
+            
+        });
     });
 
     describe('POST /', ()=> {
