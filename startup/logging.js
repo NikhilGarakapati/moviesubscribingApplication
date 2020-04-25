b3974c4972334b16a70dadc3c9a10235e81bd542
NikhilGarakapati <nikhilgarakapati@gmail.com>
2020-04-25 22:31:53 +0530

    added logging structure

--- startup/logging.js
+++ startup/logging.js
@@ -2,24 +2,35 @@ const winston = require('winston');
 require('winston-mongodb');
 require('express-async-errors');
 
-
-module.exports = function(){
+module.exports = function() {
 // process.on('uncaughtException', (ex)=>{
 //   winston.error(ex.message, ex);      
 //   process.exit(1);                     // To log Exceptions
-  
 // });
+
+
 winston.exceptions.handle(   
     new winston.transports.Console({ colorize:true, prettyPrint: true}),
     new winston.transports.File({ filename: 'uncaughtExceptions.log' })
   );
-  // to log unhandled promise rejections
   
-  process.on('unhandledRejection', (ex)=>{
-    throw(ex);                       // TO log promise rejection
+  process.on('unhandledRejection', (ex)=>{  // to log unhandled promise rejections
+    throw(ex);  // we are logging unhandled rejections and uncaught expections in uncaugt.log         
   });
   
   winston.add(new winston.transports.File({filename: 'logfile.log'}));
   //transport is a storage file consists of console, file, http. 
-  // winston.add(new winston.transports.MongoDB({ db: 'mongodb://localhost:27017/vidly'}));
+  winston.add(new winston.transports.MongoDB({ db: 'mongodb://localhost:27017/vidly'}));
 }
+
+
+
+// logging structure
+// 1 emmiting using process method -- uncaught exception
+// 2 logging them into uncaughtExceptions.log
+// 3 emitting using process method -- unhandled rejection
+// 4 wont store in uncaughtExceptions.log-- have to create another one
+// 5 instead throw exception from unhandled rejection, storing in uncaughtExcpetions.log
+
+// 6 logfile.log stores compile time errors
+// ```
